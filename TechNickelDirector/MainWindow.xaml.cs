2dc02fbbf1b9eef8fa50a4632bfed27c640e5681
Charlie Retzler <chalie.retzler@gmail.com>
2018-10-13 09:36:01 -0400

    Adding in menu and supporting functionality (save, load, new, and close)

--- TechNickelDirector/MainWindow.xaml.cs
+++ TechNickelDirector/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -7,14 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using Newtonsoft.Json;
 
 namespace TechNickelDirector
 {
@@ -23,22 +18,24 @@ namespace TechNickelDirector
     /// </summary>
     public partial class MainWindow : Window
     {
-        private PresentationWindow _presentationWindow;
+        private readonly PresentationWindow _presentationWindow;
         private WindowState _previousWindowState;
         private ResizeMode _resizeMode;
-        private List<Cue> _availableFiles;
-        private ObservableCollection<Cue> _actualCues = new ObservableCollection<Cue>();
+        private readonly List<Cue> _availableFiles;
+        private readonly ObservableCollection<Cue> _actualCues = new ObservableCollection<Cue>();
         private Cue _currentCue;
+        private readonly CueRepository _cueRepository;
 
         public MainWindow()
         {
             InitializeComponent();
             _presentationWindow = new PresentationWindow();
             _presentationWindow.Show();
-            _availableFiles = LoadFolder(@"Z:\Moron Report Stuff\2017 Summer - Chaos\");           
+            _availableFiles = LoadFolder(@"Z:\DropBox\Moron Report\Been A While\");           
 
             AvailableCues.ItemsSource = _availableFiles;
             ActualCues.ItemsSource = _actualCues;
+            _cueRepository = new CueRepository();
         }
 
         private List<Cue> LoadFolder(string path)
@@ -47,6 +44,10 @@ namespace TechNickelDirector
             var cues = new List<Cue>();
             foreach (var file in files)
             {
+                if (file.EndsWith(".json", StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
                 cues.Add(new Cue {FullFilename = file});
             }
 
@@ -114,24 +115,45 @@ namespace TechNickelDirector
         {
             var result = MessageBox.Show("Are you sure you want to close the application", "Close Application?", MessageBoxButton.YesNo);
 
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+                return;
+            }
+
             // show the message box here and collect the result
             _presentationWindow.Close();
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            if (AvailableCues.SelectedItem != null)
+            if (AvailableCues.SelectedItems.Count <= 0)
             {
-               _actualCues.Add((Cue) AvailableCues.SelectedItem);
+                return;
             }
+
+            foreach (var item in AvailableCues.SelectedItems)
+            {
+                _actualCues.Add((Cue)item);
+            }
+
+            AvailableCues.SelectedItems.Clear();
         }
 
         private void RemoveButton_Click(object sender, RoutedEventArgs e)
         {
-            if (ActualCues.SelectedIndex != -1)
+            if (ActualCues.SelectedItems.Count <= 0)
             {
-                _actualCues.RemoveAt(ActualCues.SelectedIndex);
+                return;
             }
+
+            var items = ActualCues.SelectedItems.Cast<Cue>().ToList();
+            foreach (var item in items)
+            {
+                _actualCues.Remove((Cue)item);
+            }
+
+            ActualCues.SelectedItems.Clear();
         }
 
         private void MoveUpButton_Click(object sender, RoutedEventArgs e)
@@ -154,6 +176,58 @@ namespace TechNickelDirector
                 var index = ActualCues.SelectedIndex;
                 _actualCues.Move(index, index + 1);
             }
+        }
+
+        private void MenuNew_OnClick(object sender, RoutedEventArgs e)
+        {
+            _actualCues.Clear();
+        }
+
+        private async void MenuSave_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog
+            {
+                DefaultExt = "json",
+                Filter = "Json Files (*.json)|*.json"
+            };
+
+            var result = dialog.ShowDialog();
+
+            if (result.HasValue == false || result.Value == false)
+            {
+                return;
+            }
+
+            // TODO: Add Error Handling
+            await _cueRepository.SaveToFile(dialog.FileName, _actualCues);
+        }
+
+        private void MenuOpen_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                DefaultExt = "json",
+                Filter = "Json Files (*.json)|*.json"
+            };
+
+            var result = dialog.ShowDialog();
+
+            if (result.HasValue == false || result.Value == false)
+            {
+                return;
+            }
+
+            // TODO: Add Error Handling
+            var cues = _cueRepository.LoadFromFile(dialog.FileName);
+
+            // TOOD: Should we verify loaded files still exist?
+
+            _actualCues.Clear();
+            _actualCues.AddRange(cues);
+        }
+        private void MenuExit_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
---
+++ TechNickelDirector/ObservableCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace TechNickelDirector
+{
+    public static class ObservableCollectionExtensions
+    {
+        public static void AddRange<T>(this ObservableCollection<T> coll, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                coll.Add(item);
+            }
+        }
+    }
+}
